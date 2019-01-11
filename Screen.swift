55235f6abec9f6856344f77f00d691664c4c3397
Oleksii Mykhailenko <ole.mykhailenko@temabit.com>
2019-01-11 18:12:09 +0200

    Added "Screen.scale" and "Screen.aspectRatio". The last one always 0.x

--- Screen.swift
+++ Screen.swift
@@ -13,8 +13,12 @@ extension IOSDevice {
         
         public static let width: CGFloat = UIScreen.main.bounds.width
         public static let height: CGFloat = UIScreen.main.bounds.height
+        public static let scale: CGFloat = UIScreen.main.scale
         public static let resolution: String = "\(width)x\(height)"
-        
+        public static var aspectRatio: CGFloat {
+                return min(width, height)/max(width, height)
+        }
+
         // 0 - 100
         public static var brightness: UInt8 {
             get {
